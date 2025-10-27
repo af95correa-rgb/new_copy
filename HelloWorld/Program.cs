dc93e8713ad1ae4ad1ecebedea889f50ee21fea8
af95correa-rgb <af95correa@gmail.com>
2025-10-27 17:12:47 -0500

    Change Hello World by Class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            string Name = "Jhon";
+            Name = "1Jhon";
+            Console.WriteLine($"Hello World {Name}");
+            Console.WriteLine();
+        }
+    }
+}
